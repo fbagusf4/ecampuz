dd794f169337facbffd9b284f1ce3fd873946878
Febri Bagus Ananda <50182564+fbagusf4@users.noreply.github.com>
2021-09-14 11:00:33 +0700

    ArrayLoop

---
+++ ArrayLoop.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+$aplikasi[1] ='gtAkademik';
+$aplikasi[2] ='gtFinansi';
+$aplikasi[3] ='gtPerizinan';
+$aplikasi[4] ='eCampuz';
+$aplikasi[5] ='eOviz';
+
+$x=1;
+
+while ($x <= 5) {
+    echo"$x . $aplikasi[$x] <br>";
+$x++;
+}
+?>  
+
+</body>
+</html>
